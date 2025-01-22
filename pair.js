0d392c0cbc23b887d56b597dc010d0af34841960
devilking8900 <hyperkingoff@outlook.com>
2025-01-22 12:51:31 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
+*_Pair Code Connected by BLACK DEVIL*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
+║ *『 WOW YOU'VE CHOSEN DEVIL XD』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+║❒ *Ytube:* _youtube.com/@freetricksdevil78_
+║❒ *Owner:* _https://wa.me/917668337116_
+║❒ *Repo:* _https://github.com/DEVIL-YTS
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaId1xBDOQIfNsWQwv46
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaId1xBDOQIfNsWQwv46
+║❒ *Plugins:* _https://github.com/DeViiLXD
 ╚════════════════════════╝
 _____________________________________
 

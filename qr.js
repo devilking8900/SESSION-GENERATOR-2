508010eb8fff063d39a9908740bf6a6f55b9427e
devilking8900 <hyperkingoff@outlook.com>
2025-01-22 12:54:02 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *DEVIL-XD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/DEVIL-YTS_
+❒ *Author:* _wa.me/917668337116_
+❒ *YT:* _https://youtube.com/@freetricksdevil78_
 └────────────⭓
 `
 
